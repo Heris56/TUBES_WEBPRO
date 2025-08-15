cbbc57141a1f45613d4673d4bb7ccbf5b7335ec5
darryl <darrylrambi@gmail.com>
2025-08-15 08:23:06 +0700

    added keranjang factory

--- database/factories/KeranjangFactory.php
+++ database/factories/KeranjangFactory.php
@@ -17,7 +17,12 @@ class KeranjangFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'total' => $this->faker->randomFloat(2, 10, 1000),
+            'kuantitas' => $this->faker->numberBetween(1, 10),
+            'status' => $this->faker->randomElement(['Pending', 'Paid', 'Cancelled']),
+            'id_pembeli' => \App\Models\Pembeli::factory(),
+            'id_produk' => \App\Models\Produk::factory(),
+            'id_batch' => $this->faker->numberBetween(1, 1000),
         ];
     }
 }

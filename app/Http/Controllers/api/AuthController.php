648864f40d9bcbc3d21d5ff62cf0eebf24072a54
darryl <darrylrambi@gmail.com>
2025-08-19 01:23:51 +0700

    fix messages

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -266,7 +266,7 @@ class AuthController extends Controller
         ]);
         return $this->success([
             'user' => $user,
-            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken
+            'token' => $user->createToken('API token of ' . $user->username)->plainTextToken
         ]);
     }
 

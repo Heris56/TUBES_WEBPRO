96c6eddc2a2fed4fd14267e2620af9c736cba561
darryl <darrylrambi@gmail.com>
2025-08-19 12:46:24 +0700

    finished login

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -10,6 +10,7 @@ use App\Models\Umkm;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
@@ -273,6 +274,17 @@ class AuthController extends Controller
 
     public function login(LoginUserRequest $request)
     {
-        return 'Login success';
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('', 'Invalid credentials', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->username)->plainTextToken
+        ]);
     }
 }
